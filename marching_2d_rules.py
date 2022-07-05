b8fad7d6d6be038e62ecd522142936a27a5b2f8b
syafiqq <id.muhammad.syafiq@gmail.com>
2022-07-06 03:26:38 +0700

    add to points

--- marching_2d_rules.py
+++ marching_2d_rules.py
@@ -508,3 +508,253 @@ def to_poly(points, point_type):
 
         ]).flatten()
         return pv.PolyData(points, faces=faces)
+
+
+def to_points(points, point_type):
+    if point_type == 0:
+        # 0 0 0 0 ~ 0
+        #
+        # . . .
+        # . . .
+        # . . .
+        points = []
+        return points
+
+    elif point_type == 8:
+        # 1 0 0 0 ~ 8
+        #
+        # . . .
+        # . . .
+        # 1 . .
+        p0 = points[0]
+        p1 = __mid_point(points[0], points[1])
+        p2 = __mid_point(points[0], points[2])
+
+        points = [p0, p1, p2]
+        return points
+
+    elif point_type == 4:
+        # 0 1 0 0 ~ 4
+        #
+        # . . .
+        # . . .
+        # . . 1
+        p0 = __mid_point(points[0], points[1])
+        p1 = points[1]
+        p2 = __mid_point(points[1], points[3])
+
+        points = [p0, p1, p2]
+        return points
+
+    elif point_type == 2:
+        # 0 0 1 0 ~ 2
+        #
+        # 1 . .
+        # . . .
+        # . . .
+        p0 = __mid_point(points[0], points[2])
+        p1 = points[2]
+        p2 = __mid_point(points[2], points[3])
+
+        points = [p0, p1, p2]
+        return points
+
+    elif point_type == 1:
+        # 0 0 0 1 ~ 1
+        #
+        # . . 1
+        # . . .
+        # . . .
+        p0 = __mid_point(points[1], points[3])
+        p1 = __mid_point(points[2], points[3])
+        p2 = points[3]
+
+        points = [p0, p1, p2]
+        return points
+
+    elif point_type == 12:
+        # 1 1 0 0 ~ 12
+        #
+        # . . .
+        # . . .
+        # 1 . 1
+        p0 = points[0]
+        p2 = points[1]
+        p1 = __mid_point(points[0], points[1])
+        p3 = __mid_point(points[0], points[2])
+        p5 = __mid_point(points[1], points[3])
+        p4 = __mid_point(p3, p5)
+
+        points = [p0, p1, p2, p3, p4, p5]
+        return points
+
+    elif point_type == 10:
+        # 1 0 1 0 ~ 10
+        #
+        # 1 . .
+        # . . .
+        # 1 . .
+        p0 = points[0]
+        p1 = __mid_point(points[0], points[1])
+        p2 = __mid_point(points[0], points[2])
+        p3 = __mid_point(p2, __mid_point(points[1], points[3]))
+        p4 = points[2]
+        p5 = __mid_point(points[2], points[3])
+
+        points = [p0, p1, p2, p3, p4, p5]
+        return points
+
+    elif point_type == 3:
+        # 0 0 1 1 ~ 3
+        #
+        # 1 . 1
+        # . . .
+        # . . .
+        p0 = __mid_point(points[0], points[2])
+        p2 = __mid_point(points[1], points[3])
+        p1 = __mid_point(p0, p2)
+        p3 = points[2]
+        p4 = __mid_point(points[2], points[3])
+        p5 = points[3]
+
+        points = [p0, p1, p2, p3, p4, p5]
+        return points
+
+    elif point_type == 5:
+        # 0 1 0 1 ~ 5
+        #
+        # . . 1
+        # . . .
+        # . . 1
+        p0 = __mid_point(points[0], points[1])
+        p1 = points[1]
+        p3 = __mid_point(points[1], points[3])
+        p2 = __mid_point(__mid_point(points[0], points[2]), p3)
+        p4 = __mid_point(points[2], points[3])
+        p5 = points[3]
+
+        points = [p0, p1, p2, p3, p4, p5]
+        return points
+
+    elif point_type == 9:
+        # 1 0 0 1 ~ 9
+        #
+        # . . 1
+        # . . .
+        # 1 . .
+        p0 = points[0]
+        p1 = __mid_point(points[0], points[1])
+        p2 = __mid_point(points[0], points[2])
+        p3 = __mid_point(points[1], points[3])
+        p4 = __mid_point(points[2], points[3])
+        p5 = points[3]
+
+        points = [p0, p1, p2, p3, p4, p5]
+        return points
+
+    elif point_type == 6:
+        # 0 1 1 0 ~ 6
+        #
+        # 1 . .
+        # . . .
+        # . . 1
+        p0 = __mid_point(points[0], points[1])
+        p1 = points[1]
+        p2 = __mid_point(points[1], points[3])
+        p3 = __mid_point(points[0], points[2])
+        p4 = points[2]
+        p5 = __mid_point(points[2], points[3])
+
+        points = [p0, p1, p2, p3, p4, p5]
+        return points
+
+    elif point_type == 14:
+        # 1 1 1 0 ~ 14
+        #
+        # 1 . .
+        # . . .
+        # 1 . 1
+        p0 = points[0]
+        p1 = __mid_point(points[0], points[1])
+        p2 = points[1]
+        p3 = __mid_point(points[0], points[2])
+        p5 = __mid_point(points[1], points[3])
+        p4 = __mid_point(p3, p5)
+        p6 = points[2]
+        p7 = __mid_point(points[2], points[3])
+
+        points = [p0, p1, p2, p3, p4, p5, p6, p7]
+        return points
+
+    elif point_type == 13:
+        # 1 1 0 1 ~ 13
+        #
+        # . . 1
+        # . . .
+        # 1 . 1
+        p0 = points[0]
+        p1 = __mid_point(points[0], points[1])
+        p2 = points[1]
+        p3 = __mid_point(points[0], points[2])
+        p5 = __mid_point(points[1], points[3])
+        p4 = __mid_point(p3, p5)
+        p6 = __mid_point(points[2], points[3])
+        p7 = points[3]
+
+        points = [p0, p1, p2, p3, p4, p5, p6, p7]
+        return points
+
+    elif point_type == 7:
+        # 0 1 1 1 ~ 7
+        #
+        # 1 . 1
+        # . . .
+        # . . 1
+        p0 = __mid_point(points[0], points[1])
+        p1 = points[1]
+        p2 = __mid_point(points[0], points[2])
+        p4 = __mid_point(points[1], points[3])
+        p3 = __mid_point(p2, p4)
+        p5 = points[2]
+        p6 = __mid_point(points[2], points[3])
+        p7 = points[3]
+
+        points = [p0, p1, p2, p3, p4, p5, p6, p7]
+        return points
+
+    elif point_type == 11:
+        # 1 0 1 1 ~ 1
+        #
+        # 1 . 1
+        # . . .
+        # 1 . .
+        p0 = points[0]
+        p1 = __mid_point(points[0], points[1])
+        p2 = __mid_point(points[0], points[2])
+        p4 = __mid_point(points[1], points[3])
+        p3 = __mid_point(p2, p4)
+        p5 = points[2]
+        p6 = __mid_point(points[2], points[3])
+        p7 = points[3]
+
+        points = [p0, p1, p2, p3, p4, p5, p6, p7]
+        return points
+
+    elif point_type == 15:
+        # 1 1 1 1 ~ 15
+        #
+        # 1 . 1
+        # . . .
+        # 1 . 1
+        p0 = points[0]
+        p1 = __mid_point(points[0], points[1])
+        p2 = points[1]
+        p3 = __mid_point(points[0], points[2])
+        p5 = __mid_point(points[1], points[3])
+        p4 = __mid_point(p3, p5)
+        p6 = points[2]
+        p7 = __mid_point(points[2], points[3])
+        p8 = points[3]
+
+        points = [p0, p1, p2, p3, p4, p5, p6, p7, p8]
+        return points
