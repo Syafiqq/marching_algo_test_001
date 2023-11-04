2bcca8d59d0c5fabe0c5d4e2579a98c0efc96f2b
syafiqq <id.muhammad.syafiq@gmail.com>
2023-11-04 08:06:25 +0700

    SFQ-02: 002: Add dat sanitiser 1 example

--- baked/marching_cubes_dw_dat_sanitiser_1.py
+++ baked/marching_cubes_dw_dat_sanitiser_1.py
@@ -9,5 +9,45 @@ def main(input_path: str, output_path: str, little_endian: bool):
     print(dimension)
 
 
+def example_sq_object():
+    main(
+        '../input/sq_object.dat',
+        '../input/sq_object_sanitised.txt',
+        True
+    )
+
+
+def example_stagbettle_208x208x123():
+    main(
+        '../input/stagbeetle208x208x123.dat',
+        '../input/stagbeetle208x208x123_sanitised.txt',
+        True
+    )
+
+
+def example_stagbettle_832x832x494():
+    main(
+        '../input/stagbeetle832x832x494.dat',
+        '../input/stagbeetle832x832x494_sanitised.txt',
+        True
+    )
+
+
+def example_stagbettle_277x277x164():
+    main(
+        '../input/stagbeetle277x277x164.dat',
+        '../input/stagbeetle277x277x164_sanitised.txt',
+        True
+    )
+
+
+def example_stagbettle_416x416x247():
+    main(
+        '../input/stagbeetle416x416x247.dat',
+        '../input/stagbeetle416x416x247_sanitised.txt',
+        True
+    )
+
+
 if __name__ == "__main__":
     pass
