3a2c4a8853da39ddd2843b35766c441ea2ebc3e3
syafiqq <id.muhammad.syafiq@gmail.com>
2023-11-04 17:20:04 +0700

    SFQ-02: 003: Add tif builder example

--- baked/marching_cubes_dw_tif_builder_1.py
+++ baked/marching_cubes_dw_tif_builder_1.py
@@ -8,5 +8,41 @@ def main(input_path: str, output_path: str, xy_size: int, include_zero: bool):
     tif_builder.build(values, xy_size, xy_size, include_zero, output_path)
 
 
+def example_bunny_le_0():
+    main(
+        '../input/objs_bunny_sanitised.txt',
+        '../images/bunny_le_0',
+        512,
+        True
+    )
+
+
+def example_bunny_le_1():
+    main(
+        '../input/objs_bunny_sanitised.txt',
+        '../images/bunny_le_1',
+        512,
+        False
+    )
+
+
+def example_bunny_be_0():
+    main(
+        '../input/objs_bunny_sanitised_be.txt',
+        '../images/bunny_be_0',
+        512,
+        True
+    )
+
+
+def example_bunny_be_1():
+    main(
+        '../input/objs_bunny_sanitised_be.txt',
+        '../images/bunny_be_1',
+        512,
+        False
+    )
+
+
 if __name__ == "__main__":
     pass
